4338fb6d7b051b4190b3515ea69a46ea9a31776b
anibalajt <anibalajt@gmail.com>
2015-08-12 21:42:07 -0500

    login

--- fapi.js
+++ fapi.js
@@ -26,24 +26,7 @@ $(document).ready(function() {
         return false;
     });
 
-    $("#posts").change(function(){
-        var theURL = $(this).find("option:selected").text();
-        showCurrentPage(theURL, "externalWebPreviewPost");
-        $("#postLink").attr("href", "http://facebook.com/" + $(this).val());
-        return false;
-    });
 
-    $("#friendList").change(function(){
-        // Obtenemos el id de la persona que ha seleccionado el usuario
-        var selectedUserID = $("#friendList").val();
-
-        // Inicializamos el combo con los gustos del usuario seleccionado
-        facebookLikesByUserID(selectedUserID);
-        facebookPostsByUserID(selectedUserID);
-        facebookStatusByUserID(selectedUserID);
-
-        return false;
-    });
 });
 
 function clearAllOptions(controlID){
@@ -81,6 +64,11 @@ function userIsLogged(){
     FB.api('/me', function(user) {
         $("#user_information").html("<h3>Bienvenido " + user.name + "<h3>");
     });
+    FB.api('/me/friends','GET',{},function(response) {
+        console.log("amigos")
+        console.log(response)
+  }
+);
 }
 
 function facebookLoginCallBack(response){
@@ -117,79 +105,3 @@ function facebookLogin(){
 /**
 * Inicializa la lista de tus amigos
 */
-function initFacebookFriendSelect(){
-    clearAllOptions("friendList");
-    $("#friendList").append($('<option></option>').val("#").html("Seleccione.."));
-
-    FB.api('/me', function(user) {
-        console.log("___________________________________x")
-        console.log(user)
-
-        console.log("___________________________________x")
-        $("#friendList").append($('<option></option>').val(user.id).html(user.name));
-
-        FB.api('/me/friends','GET',
-          {"fields":"birthday,cover"},
-          function(response) {
-              // Insert your code here
-              console.log(response)
-          }
-        );
-    });
-
-    FB.api('/me/friends','GET',
-      {"fields":"birthday,cover"},
-      function(response) {
-          // Insert your code here
-          console.log(response)
-      }
-    );
-}
-
-function facebookLikesByUserID(user_id){
-    clearAllOptions("likes");
-
-    $("#ajaxActivityIndicator").show();
-
-    var fql1 = FB.Data.query("SELECT url FROM url_like WHERE user_id=" + user_id);
-    fql1.wait(function(rows) {
-        $('#likes').append($('<option></option>').val("#").html("Seleccione.."));
-
-        jQuery.each($(rows), function(idx, olike) {
-            $('#likes').append($('<option></option>').val(olike.url).html(olike.url));
-        });
-        $("#ajaxActivityIndicator").hide();
-    });
-}
-
-
-function facebookPostsByUserID(user_id){
-    clearAllOptions("posts");
-
-    $("#ajaxActivityIndicator").show();
-    var fql1 = FB.Data.query("SELECT link_id, owner, owner_comment, created_time, title, summary, url, picture, image_urls FROM link WHERE owner=" + user_id + " order by created_time desc");
-    fql1.wait(function(rows) {
-        console.log(rows)
-    });
-}
-
-function facebookStatusByUserID(user_id){
-    $("#status").find('li').remove().end();
-
-    $("#ajaxActivityIndicator").show();
-    var fql1 = FB.Data.query("SELECT status_id, time, message FROM status WHERE uid=" + user_id + " order by time desc");
-    fql1.wait(function(rows) {
-        jQuery.each($(rows), function(idx, ostatus) {
-            $("#status").append($("<li></li>").html("<a href='http://www.facebook.com/" + ostatus.status_id + "' target='_blank'>" + ostatus.message + "</a>"));
-        });
-        $("#ajaxActivityIndicator").hide();
-    });
-}
-
-function showCurrentPage(comboURL, target){
-    var frameURL = comboURL;
-    if (isYouTubeURL(comboURL)){
-        frameURL = "http://www.youtube.com/embed/" + getURLParameter(comboURL, "v");
-    }
-    $("#" + target).attr("src",  frameURL);
-}
