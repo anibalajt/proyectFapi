884f3b9d904d3f6f34f7c260d7d79b40dbd33d4c
anibalajt <anibalajt@gmail.com>
2015-08-14 14:11:13 -0500

    login

--- fapi.js
+++ fapi.js
@@ -143,7 +143,7 @@ function facebookLogin(){
             //     scope: 'publish_actions',
             //     return_scopes: true
             // })
-            FB.login(facebookLoginCallBack,  { scope: "user_photos, user_friends, user_posts, read_friendlists, public_profile, basic_info" })
+            FB.login(facebookLoginCallBack,  { scope: "user_photos, user_friends, user_posts, public_profile, basic_info" })
         }
     });
 }
