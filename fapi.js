6cfd0aa31d199d070e2a91bb0063214cfdb653e9
anibalajt <anibalajt@gmail.com>
2015-08-13 19:47:24 -0500

    login

--- fapi.js
+++ fapi.js
@@ -5,7 +5,14 @@ window.fbAsyncInit = function() {
       version    : 'v2.4',
      channelUrl:"http://fapi.conoz.ca/"
 });
-
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             console.log(response)
